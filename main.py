c9dfdf3453b7d7b7e17728711e6eb0bdc5d2535a
Timothée Lafay <tlclash12@gmail.com>
2025-11-12 16:47:13 +0100

    Format

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
 import os
 import sys
 from PyQt6.QtWidgets import (
-    QApplication, QWidget, QVBoxLayout, QTextEdit,
-    QLineEdit, QPushButton, QLabel, QFileDialog
+    QApplication,
+    QWidget,
+    QVBoxLayout,
+    QTextEdit,
+    QLineEdit,
+    QPushButton,
+    QLabel,
+    QFileDialog,
 )
 from PyQt6.QtCore import Qt
 from datetime import datetime
 import locale
 from pypdf import PdfMerger, PdfReader, PdfWriter
 
-from utils import (plaquette, extraire_annee, is_empty_plaquette,
-                   date_debut_exercice, doc_in_assembly_docs, 
-                   empty_plaquette, fusionner_pdfs, normalize_filename)
+from utils import (
+    plaquette,
+    extraire_annee,
+    is_empty_plaquette,
+    date_debut_exercice,
+    doc_in_assembly_docs,
+    empty_plaquette,
+    fusionner_pdfs,
+    normalize_filename,
+)
 from interface import MainWindow, LogPage, HomePage
 from database import EntrepriseDatabase as Database
 from init_db import EntrepriseDatabase as InitDatabase
 from latex import compiler_latex
 from processing import processing
 
-def main(mode = None):
-    
+
+def main(mode=None):
+
     database = Database()
     app = QApplication(sys.argv)
 
@@ -27,5 +41,6 @@ def main(mode = None):
     window.show()
 
     sys.exit(app.exec())
+
 
 main()
