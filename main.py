288593543795aaec9929b58be9da189a4b520313
Timothée Lafay <tlclash12@gmail.com>
2025-11-12 17:54:22 +0100

    Cleanup + fix workflow maybe

--- main.py
+++ main.py
@@ -1,35 +1,9 @@
-import os
 import sys
 from PyQt6.QtWidgets import (
     QApplication,
-    QWidget,
-    QVBoxLayout,
-    QTextEdit,
-    QLineEdit,
-    QPushButton,
-    QLabel,
-    QFileDialog,
 )
-from PyQt6.QtCore import Qt
-from datetime import datetime
-import locale
-from pypdf import PdfMerger, PdfReader, PdfWriter
-
-from utils import (
-    plaquette,
-    extraire_annee,
-    is_empty_plaquette,
-    date_debut_exercice,
-    doc_in_assembly_docs,
-    empty_plaquette,
-    fusionner_pdfs,
-    normalize_filename,
-)
-from interface import MainWindow, LogPage, HomePage
+from interface import MainWindow
 from database import EntrepriseDatabase as Database
-from init_db import EntrepriseDatabase as InitDatabase
-from latex import compiler_latex
-from processing import processing
 
 
 def main(mode=None):
@@ -43,4 +17,5 @@ def main(mode=None):
     sys.exit(app.exec())
 
 
-main()
+if __name__ == "__main__":
+    main()
